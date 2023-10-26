c3d26bc5d4c12466c5907be786187a94119ce829
Gabriel Olcina <gabriel.olcina@gmail.com>
2023-10-26 23:04:48 +0000

    Add counter

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function showContent() {
         const currentDate = new Date();
+        const remainingTime = targetDate - currentDate; //in milliseconds
 
-        if (currentDate >= targetDate) {
+        if (remainingTime <= 0) {
             const pre_content = document.getElementById("pre-content");
             pre_content.classList.remove("visible");
             pre_content.classList.add("hidden");
@@ -12,9 +13,17 @@ document.addEventListener("DOMContentLoaded", function() {
             const post_content = document.getElementById("post-content");
             post_content.classList.remove("hidden");
             post_content.classList.add("visible");
-
         }
+        const seconds = Math.floor((remainingTime / 1000) % 60);
+        const minutes = Math.floor((remainingTime / (1000 * 60)) % 60);
+        const hours = Math.floor((remainingTime / (1000 * 60 * 60)) % 24);
+        const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+
+        document.getElementById("days").textContent = days;
+        document.getElementById("hours").textContent = hours;
+        document.getElementById("minutes").textContent = minutes;
+        document.getElementById("seconds").textContent = seconds;
     }
 
-    setInterval(showContent, 1000); // Check every sec.
+    const show_delay = setInterval(showContent, 1000); // Updates every second.
 });
