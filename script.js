1740eb4459c08062c3f250a8ff216a95230c0068
Gabriel Olcina <gabriel.olcina@gmail.com>
2023-10-27 00:12:35 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const fechaObjetivo = new Date("2023-11-01T12:00:00"); // Cambia esto a la fecha y hora que desees.
+
+    function mostrarContenido() {
+        const ahora = new Date();
+
+        if (ahora >= fechaObjetivo) {
+            document.getElementById("contenido-oculto").style.display = "block";
+        }
+    }
+
+    setInterval(mostrarContenido, 1000); // Comprueba cada segundo.
+});
